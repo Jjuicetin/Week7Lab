9472f4867c3042ee4ef78decf478bb80eb7b12a4
Justin Gil <puppi@LapLop>
2022-10-24 17:45:11 -0600

    connectionpool

--- src/java/dataaccess/ConnectionPool.java
+++ src/java/dataaccess/ConnectionPool.java
@@ -4,11 +4,56 @@
  * and open the template in the editor.
  */
 package dataaccess;
+import java.sql.*;
+import javax.sql.DataSource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
-/**
- *
- * @author puppi
- */
+
 public class ConnectionPool {
     
+    private static ConnectionPool pool = null;
+    private static DataSource dataSource = null;
+    
+    private ConnectionPool() {
+    try {
+    InitialContext ic = new InitialContext();
+    dataSource = (DataSource) ic.lookup("java:/comp/env/jdbc/app");
+    } 
+    catch (NamingException e) {
+    System.out.println(e);
+        }
+    }
+    
+    public static synchronized ConnectionPool getInstance() {
+    if (pool == null) {
+    pool = new ConnectionPool();
+    }
+    return pool;
+    }
+    
+    public Connection getConnection() {
+    try {
+    return dataSource.getConnection();
+    } catch (SQLException e) {
+    System.out.println(e);
+    return null;
+    }
+    }
+    
+    public void freeConnection(Connection c) {
+    try {
+      c.close();
+      } catch (SQLException e) {
+      System.out.println(e);
+        }
+    }
 }
+
+//
+//Code that uses the connection pool:
+//ConnectionPool pool = ConnectionPool.getInstance();
+//Connection connection = pool.getConnection();
+
+// code that uses the connection to work with the database
+//pool.freeConnection(connection);
